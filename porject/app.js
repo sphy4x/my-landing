26b99dfef56ed1a6c5bea696131c23d2f4c5e679
serg1os_ <64107696+sergiosk222@users.noreply.github.com>
2026-02-07 19:29:32 +0200

    intro

---
+++ porject/app.js
@@ -0,0 +1,12 @@
+VANTA.WAVES({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3e3f3f,
+  zoom: 0.78
+})
