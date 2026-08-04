f00f87cb9ce40f22070c315070e236a5b377d9e8
poseidon <64107696+sergiosk222@users.noreply.github.com>
2026-08-04 20:02:26 +0300

    Remove Blueprint motion styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '../app.jsx';
 import '../styles.css';
-import '../motion.css';
-import '../motion-overrides.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
